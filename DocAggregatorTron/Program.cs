774fcbe4e5945303c3a39bfa1506a5a5986733c2
RiggsRiggs <tom.riggs@webskyltd.com>
2019-08-28 11:30:34 +0100

    Initial Commit

---
+++ DocAggregatorTron/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocAggregatorTron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
