9009499e9a72783de16aad4cf8348f0591630993
RiggsRiggs <tom.riggs@webskyltd.com>
2019-08-28 12:35:33 +0100

    Wayne interface structure set up

--- DocAggregatorTron/Program.cs
+++ DocAggregatorTron/Program.cs
@@ -1,15 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using DocAggregatorTron;
 
-namespace DocAggregatorTron
+namespace Sandbox
 {
-    class Program
+    public class DocAggregatorTron
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // this is a .NET class which is part of the diagnostics namespace
+            // useful when you want to see how long something takes to complete for example.
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            var processor = GetProcessor();
+
+            processor.DoSomeWork();
+
+            stopWatch.Stop();
+
+            // Get the elapsed time as a TimeSpan value.
+            var ts = stopWatch.Elapsed;
+
+            var elapsedTime = $"{ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}.{ts.Milliseconds / 10:00}";
+            Console.WriteLine("RunTime " + elapsedTime);
+            Console.WriteLine("the end - press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static IProcessor GetProcessor()
+        {
+            //var yesOrNo = true;
+            //
+            IProcessor processor;
+            //
+            //if (yesOrNo == true)
+            //{
+            //    processor = new TomAggregator();
+            //}
+            //else
+            //{
+            //    processor = new OtherAggregator();
+            //}
+
+            processor = new TomAggregator();
+
+
+            return processor;
         }
     }
 }
---
+++ DocAggregatorTron/TomAggregator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DocAggregatorTron
+{
+    public class TomAggregator : IProcessor
+    {
+        public void DoSomeWork()
+        {
+            Console.WriteLine("Do some effing work");
+        }
+        
+    }
+}
