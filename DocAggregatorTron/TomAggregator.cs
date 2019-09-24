68a43f79c43dc8f8ab563737467a2a22228233a1
RiggsRiggs <tom.riggs@webskyltd.com>
2019-09-24 11:42:43 +0100

    List files on screen

--- DocAggregatorTron/TomAggregator.cs
+++ DocAggregatorTron/TomAggregator.cs
@@ -3,16 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 
 namespace DocAggregatorTron
 {
     public class TomAggregator : IProcessor
     {
+        private const string WorkingDirectory = @"G:\My Drive\MyAdmin\PropertyDocAggregator\FilesHere";
         public void DoSomeWork()
         {
-            Console.WriteLine("Do some effing work");
+            //get files to be processed
+            var files = GetFiles(WorkingDirectory);
+
+            var fileInfos = files as FileInfo[] ?? files.ToArray();
+            if (!fileInfos.Any())
+                return;
+
+            foreach (var file in fileInfos)
+            {
+                Console.WriteLine($"Processing {file.Name}");
+            }
+
         }
-        
+
+        private IEnumerable<FileInfo> GetFiles(string workingDirectory)
+        {
+            var sourceDirectory = new DirectoryInfo(workingDirectory);
+            var files = sourceDirectory.GetFiles("*");
+
+            var filtered = files.Where(f => !f.Attributes.HasFlag(FileAttributes.Hidden));
+
+            return filtered;
+        }
+
     }
 }
